86afc232afc60ca96ec5d2522ce2956f3d6c246c
Aaron Borden <adborden@live.com>
2015-05-31 15:51:16 -0700

    Pull phrases into their own file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,7 @@
-console.log('meow');
 function init() {
-  var PHRASES = [
-    'is my new bicycle',
-    'picked you up at the airport',
-    'told me to tell you hello',
-    'thinks you are cute',
-    'bookmarked your website'
-  ];
-
   var idx = Math.floor(Math.random() * PHRASES.length);
-  console.log(idx);
-  $('#content').append('<span>Amanda ' + PHRASES[idx] + '</span>');
-  console.log('ok');
+  var phrase = PHRASES[idx] || 'is your new bicycle';
+  $('#content').append(phrase);
 }
+
 $(document).ready(init);
